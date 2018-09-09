fc8396d2e4424dc335d7f861aea1c15837d509fc
Lisa Lemons <me@lisa-lemons.com>
2018-09-09 12:39:11 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .hero.is-warning.is-bold.is-fullheight
+    .hero-body
+        .container
+          HelloWorld.title.is-1
+</template>
+
+<script>
+import './styles/typography.styl';
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  h1 
+    span(class='box') {{ message }}
+</template>
+
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        message: 'Hello, world'
+      };
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .box
+    border 1px dashed pink
+    padding 1.5rem
+    border-radius 15px
+</style>
+
